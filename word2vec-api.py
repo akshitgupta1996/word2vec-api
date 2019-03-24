812160d10388c2a9f1ae95710c4ca7a834bfcd42
akshitgupta1996 <akshitgupta1996@gmail.com>
2019-03-24 16:00:57 -0700

    added route for index set

--- word2vec-api.py
+++ word2vec-api.py
@@ -8,10 +8,12 @@ Example call: curl http://127.0.0.1:5000/wor2vec/n_similarity/ws1=Sushi&ws1=Shop
 
 from flask import Flask, request, jsonify
 from flask.ext.restful import Resource, Api, reqparse
+import gensim
 from gensim.models.word2vec import Word2Vec as w
+from gensim.models.keyedvectors import KeyedVectors
 from gensim import utils, matutils
 from numpy import exp, dot, zeros, outer, random, dtype, get_include, float32 as REAL,\
-     uint32, seterr, array, uint8, vstack, argsort, fromstring, sqrt, newaxis, ndarray, empty, sum as np_sum
+    uint32, seterr, array, uint8, vstack, argsort, fromstring, sqrt, newaxis, ndarray, empty, sum as np_sum
 import cPickle
 import argparse
 import base64
@@ -29,17 +31,21 @@ def filter_words(words):
 class N_Similarity(Resource):
     def get(self):
         parser = reqparse.RequestParser()
-        parser.add_argument('ws1', type=str, required=True, help="Word set 1 cannot be blank!", action='append')
-        parser.add_argument('ws2', type=str, required=True, help="Word set 2 cannot be blank!", action='append')
+        parser.add_argument('ws1', type=str, required=True,
+                            help="Word set 1 cannot be blank!", action='append')
+        parser.add_argument('ws2', type=str, required=True,
+                            help="Word set 2 cannot be blank!", action='append')
         args = parser.parse_args()
-        return model.n_similarity(filter_words(args['ws1']),filter_words(args['ws2']))
+        return model.n_similarity(filter_words(args['ws1']), filter_words(args['ws2']))
 
 
 class Similarity(Resource):
     def get(self):
         parser = reqparse.RequestParser()
-        parser.add_argument('w1', type=str, required=True, help="Word 1 cannot be blank!")
-        parser.add_argument('w2', type=str, required=True, help="Word 2 cannot be blank!")
+        parser.add_argument('w1', type=str, required=True,
+                            help="Word 1 cannot be blank!")
+        parser.add_argument('w2', type=str, required=True,
+                            help="Word 2 cannot be blank!")
         args = parser.parse_args()
         return model.similarity(args['w1'], args['w2'])
 
@@ -47,9 +53,12 @@ class Similarity(Resource):
 class MostSimilar(Resource):
     def get(self):
         parser = reqparse.RequestParser()
-        parser.add_argument('positive', type=str, required=False, help="Positive words.", action='append')
-        parser.add_argument('negative', type=str, required=False, help="Negative words.", action='append')
-        parser.add_argument('topn', type=int, required=False, help="Number of results.")
+        parser.add_argument('positive', type=str, required=False,
+                            help="Positive words.", action='append')
+        parser.add_argument('negative', type=str, required=False,
+                            help="Negative words.", action='append')
+        parser.add_argument('topn', type=int, required=False,
+                            help="Number of results.")
         args = parser.parse_args()
         pos = filter_words(args.get('positive', []))
         neg = filter_words(args.get('negative', []))
@@ -59,7 +68,7 @@ class MostSimilar(Resource):
         t = 10 if t == None else t
         print "positive: " + str(pos) + " negative: " + str(neg) + " topn: " + str(t)
         try:
-            res = model.most_similar_cosmul(positive=pos,negative=neg,topn=t)
+            res = model.most_similar_cosmul(positive=pos, negative=neg, topn=t)
             return res
         except Exception, e:
             print e
@@ -69,7 +78,8 @@ class MostSimilar(Resource):
 class Model(Resource):
     def get(self):
         parser = reqparse.RequestParser()
-        parser.add_argument('word', type=str, required=True, help="word to query.")
+        parser.add_argument('word', type=str, required=True,
+                            help="word to query.")
         args = parser.parse_args()
         try:
             res = model[args['word']]
@@ -78,6 +88,7 @@ class Model(Resource):
         except Exception, e:
             print e
             return
+
 
 class ModelWordSet(Resource):
     def get(self):
@@ -88,21 +99,39 @@ class ModelWordSet(Resource):
             print e
             return
 
+
+class ModelWordRank(Resource):
+    def get(self):
+        try:
+            w_rank = {}
+            for i, word in enumerate(model.index2word):
+                w_rank[word] = i
+            WORDS = w_rank
+            res = base64.b64encode(cPickle.dumps(WORDS))
+            return res
+        except Exception, e:
+            print e
+            return
+
+
 app = Flask(__name__)
 api = Api(app)
+
 
 @app.errorhandler(404)
 def pageNotFound(error):
     return "page not found"
 
+
 @app.errorhandler(500)
 def raiseError(error):
     return error
 
+
 if __name__ == '__main__':
     global model
 
-    #----------- Parsing Arguments ---------------
+    # ----------- Parsing Arguments ---------------
     p = argparse.ArgumentParser()
     p.add_argument("--model", help="Path to the trained model")
     p.add_argument("--binary", help="Specifies the loaded model is binary")
@@ -118,10 +147,13 @@ if __name__ == '__main__':
     port = int(args.port) if args.port else 5000
     if not args.model:
         print "Usage: word2vec-apy.py --model path/to/the/model [--host host --port 1234]"
-    model = w.load_word2vec_format(model_path, binary=binary)
+    # model = w.load_word2vec_format(model_path, binary=binary)
+    model = gensim.models.KeyedVectors.load_word2vec_format(
+        'GoogleNews-vectors-negative300.bin', binary=True, limit=600000)
     api.add_resource(N_Similarity, path+'/n_similarity')
     api.add_resource(Similarity, path+'/similarity')
     api.add_resource(MostSimilar, path+'/most_similar')
     api.add_resource(Model, path+'/model')
     api.add_resource(ModelWordSet, '/word2vec/model_word_set')
+    api.add_resource(ModelWordRank, '/word2vec/model_word_rank')
     app.run(host=host, port=port)
